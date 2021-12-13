24a3ff205fc1969430a4f1768c3ff228e369da4e
pashasetuhova <90975237+pashasetuhova@users.noreply.github.com>
2021-12-14 00:20:59 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,60 @@
+const hamburger = document.querySelector('.hamburger'),
+        menu = document.querySelector('.menu'),
+        close =document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+close.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+const counter = document.querySelectorAll('.skills__rating-counter'),
+        line = document.querySelectorAll('.skills__rating-line span');
+
+/* console.log(counter); */
+/* в девтулс можно проверить все захваченные элементы */
+
+counter.forEach( (item, i) => {
+    line [i].style.width = item.innerHTML;
+}); 
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+        $('.pageup').fadeIn();
+    } else {
+        $('.pageup').fadeOut();
+    }
+});
+
+$("a[href=#up]").click(function(){
+    const _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+});
+
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+  
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+  
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+  });
